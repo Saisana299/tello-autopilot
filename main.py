5722c51baf369936636e2afce9ec6f4144faf4fd
Zakki0925224 <zakki0925224@gmail.com>
2023-09-07 15:50:37 +0900

    [Add]: Added test commands

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
 from tello import Tello
 
 t = Tello()
+
+t.send_command("speed?")
+t.send_command("battely?")
+t.send_command("time?")
+t.send_command("wifi?")
+t.send_command("sdk?")
+t.send_command("sn?")
+
+t.send_command("takeoff")
+t.send_command("up 20")
+t.send_command("left 20")
+t.send_command("right 20")
+t.send_command("down 20")
+t.send_command("flip f")
+t.send_command("land")
+t.send_command("streamoff")
+
+for l in t.get_log():
+    print(l)
